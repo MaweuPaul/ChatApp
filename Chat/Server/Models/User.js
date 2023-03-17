4652d0e933de13ba268345305df74b07d2d72ae9
MaweuPaul <maweupaul85@gmail.com>
2023-03-17 13:17:21 +0300

    mongoose schema

--- Chat/Server/Models/User.js
+++ Chat/Server/Models/User.js
@@ -1,39 +1,45 @@
-import mongoose, { Mongoose } from "mongoose";
+import mongoose from "mongoose";
 
-const UserSchema = new Mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
-    min: 2,
-    max: 50,
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    firstName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    Email: {
+      type: String,
+      required: true,
+      max: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 5,
+    },
+    password: {
+      type: String,
+      default: "",
+    },
+    friends: {
+      type: Array,
+      default: [],
+    },
+    location: String,
+    occupation: String,
+    viewedProfile: Number,
+    impressions: Number,
   },
-  firstName: {
-    type: String,
-    required: true,
-    min: 2,
-    max: 50,
-  },
-  Email: {
-    type: String,
-    required: true,
-    max: 50,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    min: 5,
-  },
-  password: {
-    type: String,
-    default: "",
-  },
-  friends: {
-    type: Array,
-    default: [],
-  },
-  location: String,
-  occupation: String,
-  viewedProfile: Number,
-  impressions: Number,
-});
+  { timestamps: true }
+);
+
+const User = mongoose.model("user", UserSchema);
+export default User;
