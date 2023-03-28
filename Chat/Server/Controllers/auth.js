6c690e2da68f1779253352f4f6480cf256b894b4
MaweuPaul <maweupaul85@gmail.com>
2023-03-28 08:27:19 +0300

    ..

--- Chat/Server/Controllers/auth.js
+++ Chat/Server/Controllers/auth.js
@@ -1,10 +1,12 @@
+// Import necessary packages and modules
 import bcrypt from "bcrypt";
 import { Jwt } from "jsonwebtoken";
 import User from "../Models/User";
 
-// register user
+// Register a new user
 export const register = async (req, res) => {
   try {
+    // Extract user information from the request body
     const {
       fisrtName,
       lastName,
@@ -16,9 +18,11 @@ export const register = async (req, res) => {
       occupation,
     } = req.body;
 
+    // Generate a salt and hash the password
     const salt = await bcrypt.genSalt();
     const passwordHash = await bcrypt.hash(password, salt);
 
+    // Create a new user object with the hashed password
     const newUser = new User({
       fisrtName,
       lastName,
@@ -31,9 +35,40 @@ export const register = async (req, res) => {
       viewedProfile: Math.floor(Math.random() * 10000),
       impressions: Math.floor(Math.random() * 10000),
     });
+
+    // Save the user object to the database and return it as a JSON response
     const savedUser = await newUser.save();
     res.status(201).json(savedUser);
   } catch (err) {
-    register.status(500).json({ error: err.message });
+    // Return an error response if there was an error during registration
+    res.status(500).json({ error: err.message });
+  }
+};
+
+// Log in a user
+export const login = async (req, res) => {
+  try {
+    // Extract email and password from the request body
+    const { email, password } = req.body;
+
+    // Check if a user with the provided email exists in the database
+    const user = await User.findOne({ email: email });
+    if (!user) return res.status(400).json({ msg: "user does not exist" });
+
+    // Compare the provided password with the hashed password in the user object
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ msg: "invalid credentials" });
+
+    // If the passwords match, generate a JSON Web Token (JWT)
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+    // Remove the password from the user object using the "delete" keyword
+    delete user.password;
+
+    // Return a JSON response with the JWT and the user object (without the password)
+    res.status(200).json({ token, user });
+  } catch (error) {
+    // Return an error response if there was an error during login
+    res.status(500).json({ error: error.message });
   }
 };
